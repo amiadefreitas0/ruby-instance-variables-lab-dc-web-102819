16d031f7ce68a0962e625de22c686e8a564ec886
Amia Defreitas <amia.defreitas0@gmail.com>
2019-10-30 03:36:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
-  def initialize(name)
-    @name = name
+  def name(dog_name)
+    this_dogs_name = dog_name
   end
 
   def name
-    @name
+this_dogs_name
   end
 end
 
-kanye = Dog.new("Kanye")
-kanye.name
+fido = Dog.new("Fido")
+fido.name
